9adda56ce3fda67a0a66a8796bdecf06eeb05c39
Ross Blair <Ross Blair>
2017-07-21 23:31:20 -0400

    All features working, JS code working, but needs clean up

--- assets/javascript/generator.js
+++ assets/javascript/generator.js
@@ -1,38 +1,83 @@
 var generateGIF = {
-  // List of topics related to the word moist
-  topics: ["moist", "dank", "humid", "misty", "muggy", "rainy", "slippery", "soaked", "sodden", "stormy", "drenched", "dripping", "drizzling", "moistened", "pouring", "raining", "soaking", "sopping", "soused", "teeming", "aqueous", "clammy", "dewy", "showery", "slimy", "slushy", "soppy", "teary", "watery"],
+  // List of topics, first word is general topic
+  topics: ["food", "apple", "avocado", "bacon", "bread", "burrito", "cake", "chips", "cream cheese", "dessert", "dough", "grapefruit", "ice cream", "ketchup", "pizza", "ravioli", "tofu"],
   // Function to create buttons
-  createButtons: function() {
+  createButtons: function () {
     $("#buttonarea").empty();
 
-    // Loop through topics and create associated buttons
-    for (var i = 0; i < generateGIF.topics.length; i++) {
-
+    // Loop through topics and create associated buttons, purposefully leaves out first element
+    for (var i = 1; i < generateGIF.topics.length; i++) {
       var topicsBtn = $("<button>");
       topicsBtn.addClass("topics-button");
-      topicsBtn.attr("search-query", generateGIF.topics[i]);
+      topicsBtn.attr("data-query", generateGIF.topics[i]);
       topicsBtn.text(generateGIF.topics[i]);
       $("#buttonarea").append(topicsBtn);
     }
   },
+  addButtons: function () {
+    //  Event Listener for adding searchable button
+    $("#add-food").on("click", function (event) {
+      event.preventDefault();
 
+      var food = $("#food-input").val().trim();
+      if ($("input").val() === "") {
+        alert("Please enter a value.");
+      } else {
+        generateGIF.topics.push(food);
+        generateGIF.createButtons();
+      }
+    });
+  },
+  displayGIFs: function () {
+    var word = $(this).attr("data-query");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + word + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-}
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+      .done(function (response) {
+        $("#gifarea").empty();
+        var results = response.data;
+        if (results === "") {
+          alert("Uh oh! You picked a weird button. Try again!");
+        }
+        console.log(response);
+        for (var i = 0; i < results.length; i++) {
+          var gifDiv = $("<div class='gif-display'>");
+          var rating = results[i].rating;
+          var p = $("<p>").text("Rating: " + rating);
+          var gifImage = $("<img>");
 
-$(document).ready(function() {
+          gifImage.attr("src", results[i].images.fixed_height_still.url);
+          gifImage.attr("data-still", results[i].images.fixed_height_still.url);
+          gifImage.attr("data-animate", results[i].images.fixed_height.url);
+          gifImage.attr("data-state", "still");
+          gifImage.addClass("gifclass");
 
+          gifDiv.append(p);
+          gifDiv.append(gifImage);
+
+          $("#gifarea").prepend(gifDiv);
+        }
+      });
+  },
+  pauseGIF: function () {
+    var state = $(this).attr("data-state");
+
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  }
+};
+
+$(document).ready(function () {
   generateGIF.createButtons();
-
-  $(".topics-button").on("click", function() {
-
-    var searchquery = $("<div>");
-
-    searchquery.addClass("gifdisplay");
-    searchquery.text($(this).attr("search-query"));
-
-    $("#gifarea").append(searchquery);
-
-  });
-
-
+  generateGIF.addButtons();
+  $(document).on("click", ".topics-button", generateGIF.displayGIFs);
+  $(document).on("click", ".gifclass", generateGIF.pauseGIF);
 });
